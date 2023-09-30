719c75cac22ff641ba843ce6dcffebceb35a9097
LCLeGoff <line.chloe.legoff@gmail.com>
2023-09-30 01:06:23 +0000

    fix run.py

--- dags/run.py
+++ dags/run.py
@@ -1,6 +1,10 @@
+import sys
+import os
+sys.path.append("./")
+
 from scripts.add_new_data import add_new_raw_data_to_clean_data
 from scripts.get_api_data import get_weather_data
-from scripts.train_models import prepare_data, compute_model_score, train_best_model
+from scripts.train_models import compute_model_score, train_best_model
 
 
 get_weather_data()
@@ -9,8 +13,7 @@ add_new_raw_data_to_clean_data('data', 20)
 add_new_raw_data_to_clean_data('fulldata', None)
 
 
-X, y = prepare_data('./clean_data/fulldata.csv')
-score_lr = compute_model_score('LinearRegression', X, y)
-score_dt = compute_model_score('DecisionTree', X, y)
-score_rf = compute_model_score('RandomForest', X, y)
-train_best_model(X, y, score_lr, score_dt, score_rf)
+score_lr = compute_model_score('LinearRegression')
+score_dt = compute_model_score('DecisionTree')
+score_rf = compute_model_score('RandomForest')
+train_best_model(score_lr, score_dt, score_rf)
