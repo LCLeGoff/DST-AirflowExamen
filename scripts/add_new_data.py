497520e165b77c345d239fb83c06abef07f2ef0e
LCLeGoff <line.chloe.legoff@gmail.com>
2023-09-30 01:03:30 +0000

    fix path to scripts

--- scripts/add_new_data.py
+++ scripts/add_new_data.py
@@ -9,8 +9,8 @@ from utils.JsonClassUtils import JsonClassUtils
 def add_new_raw_data_to_clean_data(filename, n_files):
 
     temp_data_df = JsonClassUtils().transform_data_into_csv(
-        parent_folder='./raw_files', n_files=n_files)
-    filename_complete = './clean_data/'+filename+'.csv'
+        parent_folder='/app/raw_files', n_files=n_files)
+    filename_complete = '/app/clean_data/'+filename+'.csv'
 
     if n_files:
         data_df = pd.read_csv(filename_complete)
--- scripts/get_api_data.py
+++ scripts/get_api_data.py
@@ -1,14 +1,14 @@
+import sys
+sys.path.append('/app/')
 from dotenv import load_dotenv
 import os
 import requests
 import pandas as pd
 # from airflow.models import Variable
-from pathlib import Path
 
 from utils.JsonClassUtils import JsonClassUtils
 
-dotenv_path = Path('./.env')
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
 cities = ['paris', 'london', 'washington']
@@ -17,7 +17,7 @@ cities = ['paris', 'london', 'washington']
 def get_weather_data():
 
     url = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-    dt = None
+
     res_list = []
     for city in cities:
         url_city = url.format(city=city, api_key=WEATHER_API_KEY)
@@ -26,5 +26,6 @@ def get_weather_data():
         dt = r_json['dt']
 
     time = pd.to_datetime(dt, unit='s').strftime('%Y-%m-%d %H:%M')
+    print(time)
 
-    JsonClassUtils().write_obj_json('./raw_files/%s.json' % time, res_list)
+    JsonClassUtils().write_obj_json('/app/raw_files/%s.json' % time, res_list)
